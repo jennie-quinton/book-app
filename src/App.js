36e2205c88ca492a58fa9ea70b318818094463c2
Basejumper1993 <j.quinton1@gmail.com>
2018-01-24 19:02:39 +0000

    add packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Book } from './components/Book';
+import { Header } from './components/Header';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Header />
+        <h2>Current Book</h2>
         <Book
           title="Lord of Shadows"
           author="Cassandra Clare"
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Icon } from 'react-fa';
+
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <Icon name="book" />
+          &nbsp;Reading List
+        </a>
+      </nav>
+    );
+  }
+}
